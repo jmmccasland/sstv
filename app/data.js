7372abdecdb771c8c815e269789ea2e989627dcd
John Michael McCasland <jmmccasland@gmail.com>
2017-09-30 18:59:37 -0500

    wip: Artist rows scroll on overflow.

---
+++ app/data.js
@@ -0,0 +1,24 @@
+export default {
+  playlists: [
+    {
+      artist: 'YØUTH',
+      videos: [
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        },
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        },
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        }
+      ]
+    }
+  ]
+};
--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
 import 'whatwg-fetch';
+import data from './data.js';
+
+const row = document.querySelector('.row');
+
+for (let i = 0; i < data.playlists[0].videos.length; i += 1) {
+  const rowItem = document.createElement('DIV');
+  rowItem.classList.add('row__item');
+  rowItem.innerHTML = data.playlists[0].videos[i].url;
+  row.appendChild(rowItem);
+}
