0fd16618eee324e80c07b359658114f296888393
John Michael McCasland <jmmccasland@gmail.com>
2017-10-01 14:14:36 -0500

    chore: update data to include id

--- app/data.js
+++ app/data.js
@@ -5,10 +5,12 @@ export default {
       videos: [
         {
           title: 'Hey Carolina',
+          id: 'jt1xa7uSrK4',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jt1xa7uSrK4" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Alright, Kid',
+          id: 'S1vwGCyppTE',
           url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
         }
       ]
@@ -18,10 +20,12 @@ export default {
       videos: [
         {
           title: 'Yesterday A Hero',
+          id: 'roUOa_uYYks',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/roUOa_uYYks" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Cult of My Own',
+          id: 'fLCyfIr37EY',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fLCyfIr37EY" frameborder="0" allowfullscreen></iframe>'
         }
       ]
@@ -31,18 +35,22 @@ export default {
       videos: [
         {
           title: 'Appomattox',
+          id: 'rHiPuI08SiI',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rHiPuI08SiI" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Bird\'s Eye View',
+          id: 'S32853vXVqI',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/S32853vXVqI" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Subaru',
+          id: 'v5YzqzwO1eA',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/v5YzqzwO1eA" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Matress King',
+          id: 'xEIdLvEtY1A',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/xEIdLvEtY1A" frameborder="0" allowfullscreen></iframe>'
         }
       ]
@@ -52,19 +60,8 @@ export default {
       videos: [
         {
           title: 'Pay Raise',
+          id: 'OadUAwfjJRU',
           url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/OadUAwfjJRU" frameborder="0" allowfullscreen></iframe>'
-        },
-        {
-          title: 'Bird\'s Eye View',
-          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/S32853vXVqI" frameborder="0" allowfullscreen></iframe>'
-        },
-        {
-          title: 'Subaru',
-          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/v5YzqzwO1eA" frameborder="0" allowfullscreen></iframe>'
-        },
-        {
-          title: 'Matress King',
-          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/xEIdLvEtY1A" frameborder="0" allowfullscreen></iframe>'
         }
       ]
     },
