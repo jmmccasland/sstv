7904e48d60ba9de82398e345a0c9534763a4c2c9
John Michael McCasland <jmmccasland@gmail.com>
2017-10-01 11:33:35 -0500

    feat: can generate more than one row of artist videos

--- app/data.js
+++ app/data.js
@@ -19,6 +19,26 @@ export default {
           url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
         }
       ]
+    },
+    {
+      artist: 'YØUTH',
+      videos: [
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        },
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        },
+        {
+          title: 'Alright, Kid',
+          artist: 'YØUTH',
+          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+        }
+      ]
     }
   ]
 };
--- app/index.js
+++ app/index.js
@@ -1,11 +1,29 @@
 import 'whatwg-fetch';
 import data from './data.js';
 
-const row = document.querySelector('.row');
+// for (let i = 0; i < data.playlists[0].videos.length; i += 1) {
+//   const rowItem = document.createElement('DIV');
+//   rowItem.classList.add('row__item');
+//   rowItem.innerHTML = data.playlists[0].videos[i].url;
+//   row.appendChild(rowItem);
+// }
 
-for (let i = 0; i < data.playlists[0].videos.length; i += 1) {
-  const rowItem = document.createElement('DIV');
-  rowItem.classList.add('row__item');
-  rowItem.innerHTML = data.playlists[0].videos[i].url;
-  row.appendChild(rowItem);
+
+// Create row and add playlist to row
+for (let i = 0; i < data.playlists.length; i += 1) {
+  // Create row
+  const row = document.createElement('DIV');
+  row.classList.add('row');
+  document.querySelector('.artist').appendChild(row);
+  console.log('create playlist row' + i);
+
+  for (let j = 0; j < data.playlists[i].videos.length; j += 1) {
+    const rowItem = document.createElement('DIV');
+    rowItem.classList.add('row__item');
+    rowItem.innerHTML = data.playlists[i].videos[j].url;
+    row.appendChild(rowItem);
+    console.log('create video row__item' + j);
+  }
+
+  // console.log('done creating playlist & videos' + j);
 }
