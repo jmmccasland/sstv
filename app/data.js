fec2efc87dabf58733d4260b1885824b753247a4
John Michael McCasland <jmmccasland@gmail.com>
2017-10-01 12:18:17 -0500

    feat: can scroll-x on artists with > 3 videos

--- app/data.js
+++ app/data.js
@@ -4,41 +4,44 @@ export default {
       artist: 'YØUTH',
       videos: [
         {
-          title: 'Alright, Kid',
-          artist: 'YØUTH',
-          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+          title: 'Hey Carolina',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jt1xa7uSrK4" frameborder="0" allowfullscreen></iframe>'
         },
         {
           title: 'Alright, Kid',
-          artist: 'YØUTH',
-          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
-        },
-        {
-          title: 'Alright, Kid',
-          artist: 'YØUTH',
           url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
         }
       ]
     },
     {
-      artist: 'YØUTH',
+      artist: 'Pocket Science',
       videos: [
         {
-          title: 'Alright, Kid',
-          artist: 'YØUTH',
-          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+          title: 'Yesterday A Hero',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/roUOa_uYYks" frameborder="0" allowfullscreen></iframe>'
         },
         {
-          title: 'Alright, Kid',
-          artist: 'YØUTH',
-          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
-        },
-        {
-          title: 'Alright, Kid',
-          artist: 'YØUTH',
-          url: '<iframe width=\'560\' height=\'315\' src=\'https://www.youtube.com/embed/S1vwGCyppTE\' frameborder=\'0\' allowfullscreen></iframe>'
+          title: 'Cult of My Own',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fLCyfIr37EY" frameborder="0" allowfullscreen></iframe>'
         }
       ]
-    }
+    },
+    {
+      artist: 'Lords of Liechtenstein',
+      videos: [
+        {
+          title: 'Appomattox',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rHiPuI08SiI" frameborder="0" allowfullscreen></iframe>'
+        },
+        {
+          title: 'Bird\'s Eye View',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/S32853vXVqI" frameborder="0" allowfullscreen></iframe>'
+        },
+        {
+          title: 'Subaru',
+          url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/v5YzqzwO1eA" frameborder="0" allowfullscreen></iframe>'
+        }
+      ]
+    },
   ]
 };
--- app/index.js
+++ app/index.js
@@ -9,21 +9,33 @@ import data from './data.js';
 // }
 
 
-// Create row and add playlist to row
-for (let i = 0; i < data.playlists.length; i += 1) {
-  // Create row
-  const row = document.createElement('DIV');
-  row.classList.add('row');
-  document.querySelector('.artist').appendChild(row);
-  console.log('create playlist row' + i);
+const sectionArtists = document.querySelector('.section--artists');
 
+// Loop through playlists
+for (let i = 0; i < data.playlists.length; i += 1) {
+  const artist = document.createElement('DIV');
+  artist.classList.add('artist');
+  sectionArtists.appendChild(artist);
+
+  // Create artist name
+  const artistName = document.createElement('H2');
+  artistName.classList.add('artist__name');
+  artistName.innerText = data.playlists[i].artist;
+
+  // Create artist row
+  const row = document.createElement('DIV');
+  row.classList.add('artist__row');
+
+
+  // add row to artist div
+  artist.appendChild(artistName);
+  artist.appendChild(row);
+
+  // Loop through playlists[i].videos
   for (let j = 0; j < data.playlists[i].videos.length; j += 1) {
     const rowItem = document.createElement('DIV');
-    rowItem.classList.add('row__item');
+    rowItem.classList.add('artist__row__item');
     rowItem.innerHTML = data.playlists[i].videos[j].url;
     row.appendChild(rowItem);
-    console.log('create video row__item' + j);
   }
-
-  // console.log('done creating playlist & videos' + j);
 }
