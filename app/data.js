4b225e35ac12886ed5d5464a84c4a3fb0fe6342d
John Michael McCasland <jmmccasland@gmail.com>
2017-10-01 19:50:02 -0500

    feat: all data present

--- app/data.js
+++ app/data.js
@@ -123,5 +123,275 @@ export default {
         }
       ]
     },
+    {
+      artist: 'American Opera',
+      videos: [
+        {
+          title: 'Spoons & Knives',
+          id: 'EJj62aYFZ6M'
+        },
+        {
+          title: 'Small Victories',
+          id: 'FzE0z7Zl2K8'
+        },
+        {
+          title: 'Songs I Used To Sing',
+          id: '9HjgxxE9WFU'
+        }
+      ]
+    },
+    {
+      artist: 'Alex Fraser',
+      videos: [
+        {
+          title: 'A Song For Suzanne',
+          id: 'JhGd--7wpeY'
+        },
+        {
+          title: 'Carlisle',
+          id: 'S_B1PPC0RXY'
+        },
+        {
+          title: 'Homecoming',
+          id: 'iKv2qm1sejU'
+        },
+        {
+          title: 'Smile',
+          id: 'e3W-E91A3OU'
+        }
+      ]
+    },
+    {
+      artist: 'Formative Years',
+      videos: [
+        {
+          title: 'Wax',
+          id: 'PTG-n9NfJ_8'
+        },
+        {
+          title: 'Hospitality',
+          id: 'lvtPRCCiXdw'
+        },
+        {
+          title: 'Weight',
+          id: 'UVH_udbgK-o'
+        },
+        {
+          title: 'Free',
+          id: 'yEzI6csRE7M'
+        },
+        {
+          title: 'Motivational Speech',
+          id: 'j6Nzimkw_0s'
+        }
+      ]
+    },
+    {
+      artist: 'Carter Hulsey',
+      videos: [
+        {
+          title: 'NPR feat: American Opera',
+          id: 'wIXXZuP_TSM'
+        },
+        {
+          title: 'Heart Still Broke',
+          id: 'a1RfGXCESWU'
+        },
+        {
+          title: 'Haunt Me',
+          id: 'Yf1xLnJK1RA'
+        }
+      ]
+    },
+    {
+      artist: 'Fossil Youth',
+      videos: [
+        {
+          title: 'Watercolor Daydream',
+          id: 'zaxXYFDMrwk'
+        },
+        {
+          title: 'Common Ghost',
+          id: 'zYMF7HwnhLo'
+        },
+        {
+          title: 'Sitting in a Spinning Room',
+          id: 'mnRKDH3kET8'
+        },
+        {
+          title: 'Interview w/ The Local Wave',
+          id: 'bCFDJUYCc9A'
+        }
+      ]
+    },
+    {
+      artist: 'Looming',
+      videos: [
+        {
+          title: 'Waves',
+          id: 'jM_xUFMFU8Q'
+        },
+        {
+          title: 'Output',
+          id: 'T61pKIEPjXA'
+        },
+        {
+          title: 'Interview w/ The Local Wave',
+          id: 'Olo7MCr03DU'
+        }
+      ]
+    },
+    {
+      artist: 'West Means Home',
+      videos: [
+        {
+          title: 'Never Again',
+          id: 'BnlTfd3RIm0'
+        },
+        {
+          title: 'Invitation',
+          id: 'YsTtgJDe2LI'
+        },
+        {
+          title: 'Supposed To',
+          id: 'dk1hewuRl4Q'
+        },
+        {
+          title: 'Enemies',
+          id: '3CbwBTC0AnQ'
+        }
+      ]
+    },
+    {
+      artist: 'Daisyhead',
+      videos: [
+        {
+          title: 'Interview w/ The Local Wave',
+          id: 'DWpwiFSDz5g'
+        },
+        {
+          title: 'Never Know',
+          id: 'g4wgFQCONTU'
+        },
+        {
+          title: 'Take',
+          id: 'c4jXzlvUwZU'
+        },
+        {
+          title: 'Bodies',
+          id: 'Hd-0DORNbKY'
+        }
+      ]
+    },
+    {
+      artist: 'Julia Louise',
+      videos: [
+        {
+          title: 'Splinter',
+          id: 'sCrs3osjTG8'
+        },
+        {
+          title: 'Haze',
+          id: 'sct2Obpth1c'
+        },
+        {
+          title: 'Eaten Alive!',
+          id: 'KRdH3BKDbvc'
+        }
+      ]
+    },
+    {
+      artist: 'Latin Jazz Trio',
+      videos: [
+        {
+          title: 'The Peanut Vendor',
+          id: 'HOkUNPf2uQk'
+        },
+        {
+          title: 'In a Manner of Speaking',
+          id: 'Z7BTsoLRDi4'
+        }
+      ]
+    },
+    {
+      artist: 'Harper & The Bears',
+      videos: [
+        {
+          title: 'Birthmark',
+          id: '48HSH-qVDgY'
+        },
+        {
+          title: 'Washed Up',
+          id: '6haQ3aM--Ro'
+        },
+        {
+          title: 'Ghost',
+          id: '7YApuQVVCF8'
+        }
+      ]
+    },
+    {
+      artist: 'The Touch',
+      videos: [
+        {
+          title: 'The Connection',
+          id: 'V8S_pw0OMlc'
+        },
+        {
+          title: 'New Birthday',
+          id: 'o42ffZCPOD0'
+        },
+        {
+          title: 'I Think I\'m Ready',
+          id: '_yyPOM-iBJc'
+        },
+        {
+          title: 'Interview',
+          id: 'dj1gcmfVgzM'
+        }
+      ]
+    },
+    {
+      artist: 'Backpacks',
+      videos: [
+        {
+          title: 'Feels Like',
+          id: 'V_MSJzjM3qU'
+        },
+        {
+          title: 'Every Night',
+          id: 'RO1ZN8Hhopk'
+        },
+        {
+          title: 'Calls',
+          id: 'HglMNJPjBSs'
+        }
+      ]
+    },
+    {
+      artist: 'Mountains Like Wax',
+      videos: [
+        {
+          title: 'Control',
+          id: 'oLyXbohVvB8'
+        },
+        {
+          title: 'The Runner',
+          id: 'AiNXSH6IUR0'
+        },
+        {
+          title: 'Untitled',
+          id: 'mW-qkXBcJSU'
+        },
+        {
+          title: 'Interview',
+          id: 'J1Eelw8c36U'
+        },
+        {
+          title: 'Contingency',
+          id: 'cEVcieqDXoo'
+        }
+      ]
+    },
   ]
 };
